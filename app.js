83fcb560875c6f863219f5d3bcb0820c1bee9d2a
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-08-22 06:32:04 +0600

    added calculate total script

--- app.js
+++ app.js
@@ -1,63 +1,75 @@
-// function getIdElement(id, flag = 0) {
-//     if (!flag) return document.getElementById(id);
-//     if (flag === 1) return parseFloat(document.getElementById(id));
-//     let value = parseFloat(document.getElementById(id).value);
-//     if (isNaN(value)) {
-//         return 0;
-//     }
-//     return value;
-// }
+// common function
+function getIdElementValue(id) {
+    let value = parseFloat(document.getElementById(id).value);
+    if (isNaN(value)) {
+        return 0;
+    }
+    return value;
+}
 
 // common function
-function getIdElement(id){
+function getIdElement(id) {
     return document.getElementById(id);
 }
 
+let selectedPlayerUl = getIdElement("player-list");
 
-let selectedPlayerUl = getIdElement('player-list');
-
-{/* <li class="ps-2 py-1 list"> <span class="ps-2">Virat Kohli</span></li> */}
-
-let playerDiv = document.querySelectorAll('.player')
-for(let player of playerDiv){
-    let playerBtn = player.querySelector('#playerButton');
-    playerBtn.style.cursor="pointer";
-    playerBtn.addEventListener('click', function(){
-
-        if(selectedPlayerUl.children.length===5){
+let playerDiv = document.querySelectorAll(".player");
+for (let player of playerDiv) {
+    let playerBtn = player.querySelector("#playerButton");
+    playerBtn.style.cursor = "pointer";
+    playerBtn.addEventListener("click", function () {
+        if (selectedPlayerUl.children.length === 5) {
             alert("You can't add more than 5 players");
             return;
         }
-        
-        playerBtn.style.backgroundColor='rgb(49, 49, 73)';
-        playerBtn.setAttribute('disabled', true);
-        
 
-        let playerName = player.querySelector('#playerName');
+        playerBtn.style.backgroundColor = "rgb(49, 49, 73)";
+        playerBtn.setAttribute("disabled", true);
+
+        let playerName = player.querySelector("#playerName");
         let nameText = playerName.innerText;
-        
-        let span = document.createElement('span');
-        let li   = document.createElement('li');
-        li.className="ps-2 py-1 list";
-        span.className="ps-2";
+
+        let span = document.createElement("span");
+        let li = document.createElement("li");
+        li.className = "ps-2 py-1 list";
+        span.className = "ps-2";
         span.innerText = nameText;
         li.appendChild(span);
 
         selectedPlayerUl.appendChild(li);
-
-    })
+    });
 }
 
-// for(let playerbtn of playerButtons){
-//     playerbtn.addEventListener('click', function(){
-//         // let parentPlayer = playerbtn.parentNode.getElementById('playerName');
-//         // playerbtn.innerText.style.color="red";
-//         let text = playerbtn.innerText;
-//         text.style.color = 'red';
-//         // let newPlayer = playerbtn.previousSibling;
-//         // console.log(newPlayer.innerText);
-//         // let span = document.createElement('span');
-//         // span.className="ps-2";
-//     });
-// }
+let allInputs = document.querySelectorAll("input");
+for (let input of allInputs) {
+    input.addEventListener("keyup", function () {
+        if (isNaN(input.value) === true) {
+            alert("Sorry! Only Positive Number is allowed");
+            input.value = "";
+        } else if (parseFloat(input.value) < 0) {
+            alert("Sorry! Negative number is not allowed");
+            input.value = "";
+        }
+    });
+}
 
+getIdElement("calculate-btn").addEventListener("click", function () {
+    let playerCostText = getIdElement("player-expenses");
+    let perPlayerPrice = parseFloat(getIdElementValue("player-price"));
+
+    let totalAddedPlayer = parseFloat(selectedPlayerUl.children.length);
+
+
+    playerCostText.innerText = "$"+ (totalAddedPlayer * perPlayerPrice);
+});
+
+
+getIdElement("calculate-total-btn").addEventListener("click", function () {
+    let onlyPlayerCost = parseFloat(getIdElement("player-expenses").innerText.split('$')[1]);
+    let managerCost = parseFloat(getIdElementValue("manager-cost"));
+    let coachCost = parseFloat(getIdElementValue("coach-cost"));
+    let totalPrice = getIdElement('total');
+
+    totalPrice.innerText = "$" + (onlyPlayerCost + managerCost + coachCost)
+});
